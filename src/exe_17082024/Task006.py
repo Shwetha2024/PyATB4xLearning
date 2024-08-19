40f26fbc22d0677fa3fe6967340cae1ce033793e
Shwetha2024 <shwethamijith@gmail.com>
2024-08-19 23:45:17 +0530

    Task 6 upload

---
+++ src/exe_17082024/Task006.py
@@ -0,0 +1,14 @@
+
+### Task #6
+'''Develop a Python script that calculates the square and cube of a given number'''
+
+R1= int(input("Enter the number"))
+print (R1)
+squ=pow(R1,2)
+print ("The square of ", R1, "is", squ)
+cube=pow(R1,3)
+print ("The cube of ", R1, "is", cube)
+
+
+
+

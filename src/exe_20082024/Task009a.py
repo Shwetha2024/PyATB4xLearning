d530bc801b797f77112312f53d2b90ca2c97580b
Shwetha2024 <shwethamijith@gmail.com>
2024-09-03 12:01:37 +0530

    fizzbuzz while loop

---
+++ src/exe_20082024/Task009a.py
@@ -0,0 +1,14 @@
+#FizzBuzz program using while loop
+
+a=1
+
+while a <= 100:
+    if (a%3== 0 and a%5 ==0):
+        print("FizzBuzz")
+    elif(a%3== 0):
+        print("Fizz")
+    elif(a%5== 0):
+        print("Buzz")
+    else:
+        print(a)
+    a=a+1

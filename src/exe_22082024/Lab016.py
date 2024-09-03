15fed418168ee0d5c25b9d5de42d12445a566e61
Shwetha2024 <shwethamijith@gmail.com>
2024-09-03 18:52:22 +0530

    Break and Pass

---
+++ src/exe_22082024/Lab016.py
@@ -0,0 +1,7 @@
+#break from the loop
+
+for i in range(10):
+    print (i)
+    if i == 5:
+        break
+
---
+++ src/exe_22082024/Lab017.py
@@ -0,0 +1,8 @@
+
+# Pass- ignore the output
+
+for i in range (9):
+    if i == 4 or i == 6:
+        print(i)
+    else:
+        pass

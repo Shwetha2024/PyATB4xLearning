5a0a61aaee3bef24b3bb116ff5b79f247be9746e
Shwetha2024 <shwethamijith@gmail.com>
2024-09-03 23:36:01 +0530

    printing odd and even numbers

---
+++ src/exe_22082024/Lab018.py
@@ -0,0 +1,7 @@
+# print even/ odd numbers
+
+for i in range (20):
+    if i % 2 == 0: # != 0 condition for odd number
+        print(i)
+    else:
+        pass
---
+++ src/exe_22082024/Lab019.py
@@ -0,0 +1,6 @@
+# print even/ odd numbers
+
+for i in range (20):
+    if i % 2 == 0: # != 0 for even numbers
+        continue
+    print(i)

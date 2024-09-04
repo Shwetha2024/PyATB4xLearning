cba097c0de2e5fe0110389055877ff1aec4287cb
Shwetha2024 <shwethamijith@gmail.com>
2024-09-04 19:00:46 +0530

    Sum of 3 numbers using return

---
+++ src/exe_24082024/Lab025.py
@@ -0,0 +1,11 @@
+# Sum of 3 numbers using functions
+
+num1=int(input("Enter 1st number"))
+num2=int(input("Enter 2nd number"))
+num3=int(input("Enter 3rd number"))
+
+def sum_of_num(n1,n2,n3):
+    print (n1+n2+n3)
+
+result=sum_of_num(num1,num2,num3)
+print(result)

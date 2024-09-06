da5b126cc290ff63f4a3066d04cf4a32601097d2
Shwetha2024 <shwethamijith@gmail.com>
2024-09-06 17:58:38 +0530

    dict

---
+++ src/exe_29082024/Lab038.py
@@ -0,0 +1,29 @@
+#dict
+
+personal_info={
+    "name":"Shwetha",
+    "age":60,
+    "age":35,
+    "address":{
+        "Home":"Kerala",
+        "Office":"Ka"
+    }
+}
+
+personal_info2={
+    "name":"Mijith",
+    "age":40,
+    "address":{
+        "Home":"KAN",
+        "Office":"KAS"
+    }
+}
+print(personal_info)
+
+print(personal_info["age"])
+print(personal_info["name"])
+print(personal_info["address"])
+print(personal_info["address"]["Home"])
+
+list1=(personal_info,personal_info2)
+print(list1)

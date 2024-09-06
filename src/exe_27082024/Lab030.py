1bb77c6d020c1333ec854a7e459aee3dead62176
Shwetha2024 <shwethamijith@gmail.com>
2024-09-06 13:59:10 +0530

    decorator for time

--- src/exe_27082024/Lab030.py
+++ src/exe_27082024/Lab030.py
@@ -1,12 +1,25 @@
 #wrapper
 
 def decorate(a):
-    print("take out the vessel and vegetables")
-    print("switch on the gas")
-    a()
-    print("switch off the gas and the meal is ready")
-    return a
+    def wrapper():
+        print("take out the vessel and vegetables")
+        print("switch on the gas")
+        a()
+        print("switch off the gas and the meal is ready")
+    return wrapper()
+
+def decorate2(a):
+    def wrapper():
+        print("take out the vessel and vegetables")
+        print("switch on the gas")
+        a()
+        print("switch off the gas and the meal is ready")
+    return wrapper()
 
 @decorate
+@decorate2
 def cooking():
     print("Cook the food")
+
+
+
---
+++ src/exe_27082024/Lab031.py
@@ -0,0 +1,19 @@
+#time calculator
+import time
+
+def deco(t):
+    def wrapper():
+        print("start time of the test")
+        a=time.time()
+        print(a)
+        time.sleep(4)
+        t()
+        print("end time of the test")
+        b = time.time()
+        print(b)
+        print("Time taken for the test is",b-a)
+    return wrapper()
+
+@deco
+def execution_time():
+    print ("test run")

05b25a8a075cdd2872f0ff96dd9373aca355fb71
Shwetha2024 <shwethamijith@gmail.com>
2024-09-06 17:25:45 +0530

    set

---
+++ src/exe_29082024/Lab036.py
@@ -0,0 +1,24 @@
+#sets
+
+
+set1={1,3,5,7,4}
+set2={2,4,6,7}
+set7={5,7}
+set3=set1.union(set2)
+print (set3)
+
+set4=set1.intersection(set2)
+print(set4)
+
+set5=set1.difference(set2)
+print(set5)
+
+set6=set1.issubset(set2)
+print (set6)
+
+set8=set7.issubset(set1)
+print(set8)
+
+
+noduplicate={3,3,7,7,9,2}
+print (noduplicate)
